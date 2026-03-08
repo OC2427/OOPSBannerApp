bb73c2b412e3cbcc7073dfd391e5828ec200589d
OC2427 <om.chandrakar2427@gmail.com>
2026-03-08 16:56:34 +0530

    Add files via upload

---
+++ RA2511003010087_UC3_B1P2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(String.join(" ","   ***    ","   ***    ","****** ","     *****"));
+		System.out.println(String.join(" "," **   **   ","**   **  ","**    ** ","  **"));
+		System.out.println(String.join(" ","**     ** ","**     ** ","**     ** ","**"));
+		System.out.println(String.join(" ","**     ** ","**     ** ","**    **   ","**"));
+		System.out.println(String.join(" ","**     ** ","**     ** ","******       ","***"));
+		System.out.println(String.join(" ","**     ** ","**     ** ","**            ","  **"));
+		System.out.println(String.join(" ","**     ** ","**     ** ","**              "," **"));
+		System.out.println(String.join(" "," **   ** ","  **   **  ","**            ","  **"));
+		System.out.println(String.join(" ","   ***     ","  ***  ","  **        "," *****"));
+	}
+}
